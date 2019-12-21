7d863bfa44f3d70f76afdcc3b68d6311ce416fe6
sinhake2203 <rakeshmmmec@gmail.com>
2019-12-21 20:21:37 +0530

    sample check for project build successful

--- src/main/java/com/sinha/data/process/demoapp/controller/JobOneController.java
+++ src/main/java/com/sinha/data/process/demoapp/controller/JobOneController.java
@@ -1,4 +1,24 @@
 package com.sinha.data.process.demoapp.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/ratings")
 public class JobOneController {
+
+
+
+    @GetMapping(path="/user", produces = "application/json")
+    public List<String> ratings(){
+    return Arrays.asList("1");
+
+    }
+
+
 }
